7beddf8bd03b512169b381f3762a50480901c4da
timmymike <127740762+timmymike@users.noreply.github.com>
2023-07-31 21:19:21 +0800

    1.部分implementation統一改到Base Module 去 api 引入 2.Data共用部分，改為放在Base內。 3.Module命名規則統一

--- DatastoreLibs/src/main/java/com/timmy/datastorelibs/di/DataStoreModule.kt
+++ DatastoreLibs/src/main/java/com/timmy/datastorelibs/di/DataStoreModule.kt
@@ -1,12 +1,10 @@
 package com.timmy.datastorelibs.di
 
 import android.app.Application
-import android.content.Context
 import com.timmy.datastorelibs.repo.DataStoreRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
